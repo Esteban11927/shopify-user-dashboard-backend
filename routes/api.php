89f59ab5c7564de0c85328d2c738ff61f30d6d6a
Esteban <carlosesteban.ospina@utp.edu.co>
2023-05-01 12:23:49 -0500

    add endpoint to get csrf token for forms

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,13 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::post('/login', [AuthController::class, 'login']);
+
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/csrf-token', function() {
+    return response(null);
 });
